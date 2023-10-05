1616f6bd5405677ac77de13ea4018a697f177265
Kassahun Shimelis <kassahun.shimelis54@gmail.com>
2023-10-05 12:45:54 -0700

    additions

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,8 +2,8 @@
 if __name__ == "__main__":
     """Print the addition"""
     import sys
-    argv = len(sys.argv) - 1
+
     total = 0
-    for i in range(argv):
-        total += int(argv[i + 1])
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
     print("{}".format(total))
