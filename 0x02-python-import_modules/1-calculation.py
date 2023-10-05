a3b8fb3103d9dafa1d91ffe185e8454c541f8e65
Kassahun Shimelis <kassahun.shimelis54@gmail.com>
2023-10-05 11:50:57 -0700

    argument print

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    # Print the sum, div, mul and addition
+    """ Print the sum, div, mul and addition"""
     from calculator_1 import add, sub, mul, div
 
     a = 10
