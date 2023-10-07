67147ab3fdfbd61202a881a630335d33aa573ee8
Kassahun Shimelis <kassahun.shimelis54@gmail.com>
2023-10-07 20:58:56 +0300

    access to an element in a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    count = len(my_list[]) - i
-
-        if idx < 0 or idx > count:
-            return None
-        else:
-            return my_list[idx]
+    if idx < 0 and idx >= len(my_list[]):
+        return None
+    else:
+        return my_list[idx]
