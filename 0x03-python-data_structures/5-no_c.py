861e115b64033685e1cbdc03a690c4dfdbbc7421
Kassahun Shimelis <kassahun.shimelis54@gmail.com>
2023-10-08 09:26:45 +0300

    remove the character

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     char_to_remove = 'c'
 
     for i in my_string:
-        if my_string[i] != 'c' and my_string != 'C':
+        if i != char_to_remove and i != char_to_remove.upper():
             new_string += i
     return new_string
