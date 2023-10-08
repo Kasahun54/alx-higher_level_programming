feb11c92ce63534d2d6760029c87f44ea17f4109
Kassahun Shimelis <kassahun.shimelis54@gmail.com>
2023-10-08 09:24:04 +0300

    remove the character

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in my_string():
-        if my_string[i] == 'c' or my_string == 'C':
-            my_string.remove(my_string[i])
-    return my_string
+    char_to_remove = 'c'
+
+    for i in my_string:
+        if my_string[i] != 'c' and my_string != 'C':
+            new_string += i
+    return new_string
