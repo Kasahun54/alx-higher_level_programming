625f1f0b44995120b7614e68956724e2df3aff8f
Kassahun Shimelis <kassahun.shimelis54@gmail.com>
2023-10-09 17:00:00 +0300

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == '':
-        return None
+    if sentence == "":
+        return (0,None)
     else:
-        length = len(sentence)
-        first = sentence[0]
-        return length, first
+        return (len(sentence), sentence[0])
