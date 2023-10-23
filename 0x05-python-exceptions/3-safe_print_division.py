c0242dfb46c45f9e5c75cbe8af0b3aa5a09f0c69
Kassahun Shimelis <kassahun.shimelis54@gmail.com>
2023-10-23 10:30:12 -0700

    divide two element

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        div = a/b
+        divi = a/b
     except:
-        div = None
+        divi = None
     finally:
-        print("Inside result: {}".format(div))
-    return (div)
+        print("Inside result: {}".format(divi))
+    return (divi)
