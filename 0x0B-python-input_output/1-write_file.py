faf45da2bbebad2c9f58bd659d40b2166645af13
Kassahun Shimelis <kassahun.shimelis54@gmail.com>
2023-11-07 11:35:35 -0800

    writing text in a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Defines text writing function"""
+
+
+def write_file(filename="", text=""):
+    """Write a text to a UTF8 file
+
+    Args:
+        filename (str): name of the file 
+        text (str): text to write 
+    Returns:
+        The number of characters written in the file
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
