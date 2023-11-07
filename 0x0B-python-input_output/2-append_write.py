5b3d03c275ffebf2dcf859877160b493e00f99f9
Kassahun Shimelis <kassahun.shimelis54@gmail.com>
2023-11-07 11:40:16 -0800

    apend the text

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Defines text appending function"""
+
+
+def append_write(filename="", text=""):
+    """Appends a text at the end of a UTF8 file
+
+    Args:
+        filename (str): name of the file 
+        text (str):  text to append
+    Returns:
+        numbers of characters appended
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
