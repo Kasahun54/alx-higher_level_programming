d8bf3073586b8d17565a29829306c23f92eceed6
Kassahun Shimelis <kassahun.shimelis54@gmail.com>
2023-11-07 11:55:28 -0800

    reading json

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Defines reading JSON file function"""
+import json
+
+
+def load_from_json_file(filename):
+    """Create a Python object from the JSON file"""
+    with open(filename) as f:
+        return json.load(f)
+
