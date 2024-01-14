51a0f044a159f53ff8ad4a01d1c9e22c8a1cad7d
Kassahun Shimelis <kassahun.shimelis54@gmail.com>
2024-01-14 21:21:58 +0300

    indentetion correction]

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,21 +6,20 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
+      user = argv[1]
+      passwd = argv[2]
+      db = argv[3]
 
-        user = argv[1]
-        passwd = argv[2]
-        db = argv[3]
-
-        db_connection = MySQLdb.connect(host="localhost", port=3306, user=user,
+      db_connection = MySQLdb.connect(host="localhost", port=3306, user=user,
                                         passwd=passwd, db=db, charset="utf8")
 
-        cursor = db_connection.cursor()
+      cursor = db_connection.cursor()
 
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-        query_rows = cursor.fetchall()
+      cursor.execute("SELECT * FROM states ORDER BY id ASC")
+      query_rows = cursor.fetchall()
 
-        for row in query_rows:
-                print(row)
+      for row in query_rows:
+           print(row)
 
-        cursor.close()
-        db_connection.close()
+      cursor.close()
+      db_connection.close()
