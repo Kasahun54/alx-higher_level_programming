aeea7016e89c53877a50d8e5f78bd28f7a774625
Kassahun Shimelis <kassahun.shimelis54@gmail.com>
2024-01-14 22:05:10 +0300

    make all excutable

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,12 +7,12 @@ import sys
 
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                             passwd=sys.argv[2], db=sys.argv[3])
+                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT cities.name FROM cities\
         JOIN states ON cities.state_id = states.id\
         WHERE states.name = %s ORDER BY cities.id",
-                    (sys.argv[4],))
+                (sys.argv[4],))
     rows = cur.fetchall()
     retval = ""
     for row in range(len(rows)):
